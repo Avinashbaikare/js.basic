64144f1818ff0c6c224e0b13ab52687b7681bd2b
Avinashbaikare <avinashbaikare2@gmail.com>
2024-12-25 18:25:18 +0000

    datatType

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,11 @@
+// number=> 2 to the power of 53
+// bigint=> for big integer number
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+// undifined=>
+// symbol=>unique
+
+console.log(typeof "avinash") // if you want to know its type
+console.log(typeof age)
+console.log(typeof null)// type of null is object
