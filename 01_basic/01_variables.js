2d1ee36b85fc55accfba64cff9794ec3eab9aded
Avinashbaikare <avinashbaikare2@gmail.com>
2024-12-25 17:52:57 +0000

    uploding this file

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=55555
+let accountNam="avinsh"
+var accountPass="123"
+accountCity="pune"
+// accountId=444
+accountNam="pushpa"
+accountPass="3554"
+accountCity="chandrapur"
+console.log(accountId)
+console.table([accountId,accountPass,accountCity,accountNam])
