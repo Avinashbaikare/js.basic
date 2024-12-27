7b35aa9ce33d372c46ab0300df618e9ba2373ad4
Avinashbaikare <avinashbaikare2@gmail.com>
2024-12-27 17:28:20 +0000

    dataTypes

--- 01_basic/04_comparison.js
+++ 01_basic/04_comparison.js
@@ -4,3 +4,4 @@ console.log(4<8)
 console.log(4<=8)
 console.log(4!=4)
 
+
--- 01_basic/datatTypes_summary.js
+++ 01_basic/datatTypes_summary.js
@@ -1,10 +1,33 @@
 // primitive 
 // 7 Types: string,number,boolean,null,undefined,symbol,bigint
 
+const id= Symbol('123')
+const idval=Symbol('123')
+console.log(id===idval)
+console.log(4)
+
 //reference(non primitive)
 //array,objects,functions
 
-const id=symbol('123')
-const idval=symbol('123')
-console.log(id=idval)
-console.log(4)
+const heros=["batman","superman","spiderman"]
+let myobj={
+    name:"avinash",
+    age:23,
+}
+const myfunction=function(){
+    console.log("hello world")
+}
+console.log()
+//1) Primitive Datatypes
+    //   Number => number
+     //  String  => string
+    //   Boolean  => boolean
+    //   null  => object
+    //   undefined  =>  undefined
+    //   Symbol  =>  symbol
+    //   BigInt  =>  bigint
+
+//2) Non-primitive Datatypes
+    //   Arrays  =>  object
+    //   Function  =>  function
+    //   Object  =>  object
