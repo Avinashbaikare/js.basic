89592f363b7c6b4ea8afc016c9f5b4fe6f101d39
Avinashbaikare <avinashbaikare2@gmail.com>
2024-12-27 18:22:32 +0000

    strings

---
+++ 01_basic/05_Strings.js
@@ -0,0 +1,23 @@
+const name='avinash'
+const repocount=35
+console.log(name+repocount+" connect")
+console.log(`hello my name is ${name} and my repocount is ${repocount}`)
+
+const gamename=new String("avinsh")
+console.log(gamename)
+
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2))
+console.log(gamename.indexOf('i'))
+const newstring=gamename.substring(0,4)
+console.log(newstring)
+const newslice=gamename.slice(-7,4)
+console.log(newslice)
+const newone="     batman    *"
+console.log(newone)
+console.log(newone.trim())
+const url="avinash@gmail.gom"
+console.log(url.replace('gom','com'))
+console.log(url.includes('gmail'))
+console.log(url.split('a'))
