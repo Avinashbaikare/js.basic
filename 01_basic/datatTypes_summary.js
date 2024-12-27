c365b90302b5fc43f6ae75f234cbaa47f726e6ec
Avinashbaikare <avinashbaikare2@gmail.com>
2024-12-27 16:52:03 +0000

    datatypes_summary

---
+++ 01_basic/datatTypes_summary.js
@@ -0,0 +1,10 @@
+// primitive 
+// 7 Types: string,number,boolean,null,undefined,symbol,bigint
+
+//reference(non primitive)
+//array,objects,functions
+
+const id=symbol('123')
+const idval=symbol('123')
+console.log(id=idval)
+console.log(4)
