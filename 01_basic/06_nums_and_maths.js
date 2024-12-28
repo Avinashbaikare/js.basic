38a071caa685e58753c2260ba02768aa79819283
Avinashbaikare <avinashbaikare2@gmail.com>
2024-12-28 16:40:21 +0000

    nums

--- 01_basic/06_nums_and_maths.js
+++ 01_basic/06_nums_and_maths.js
@@ -3,3 +3,13 @@ console.log(no)
 let number=new Number(400)
 console.log(number)
 console.log(number.toString().length)
+
+//**************MATHS***************** */
+console.log(Math)
+console.log(Math.abs(-5))
+console.log(Math.round(6.6))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9))
+console.log(Math.max(4,5,2,8))
+console.log(Math.min(5,8,3,2))
+console.log(Math.round (Math.random()))
